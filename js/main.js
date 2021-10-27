c3a6ef87e982c2a4f686c2d1129d903d23ac6659
washogo <aiz4338150@gmail.com>
2021-10-28 02:22:07 +0900

    [Finish]dotInstall_BasicSyntax13/26

---
+++ js/main.js
@@ -0,0 +1,147 @@
+'use strict';
+//厳密なエラーチェックを行う命令
+
+
+
+/*if文について*/
+//if(条件式A){
+//   trueだった時の処理
+// }else if(条件式B){
+//   AがfalseでBがtrueだった時の処理
+// }else{
+//   それ以外の場合の処理
+// }
+
+// const score = 40;
+
+// if(score >= 80){
+//   console.log('Great!');
+// }else if(score >=60){
+//   console.log('Good.');
+// }else{
+//   console.log('OK...');
+// }
+
+//{}で囲った部分はブロックと呼ばれる
+//命令がブロックで終わる場合には;を付けない
+
+
+
+/* データ型について */
+// const price = 1200;
+
+// console.log(price > 1000); //true
+// console.log(price < 1000); //false
+// console.log(price >= 1000); //true
+// console.log(price <= 1000); //false
+// console.log(price === 1000); //false
+// console.log(price !== 1000); //true
+
+// false <- 0,null,undefined,'',false
+//true <- それ以外
+
+// console.log(Boolean(0));
+// console.log(Boolean('hello'));
+
+// console.log('5' * 3);
+// console.log('5' - '3');
+
+// console.log('5' + 3);
+// console.log(parseInt('5',10) + 3);
+//paseInt(X,Y)はXをY進法で数値に変換する命令
+
+// console.log(parseInt('hello',10));
+// // データ型の種類
+// // 文字列(String)
+// // 数値(Number)
+// // Undefined
+// // Null
+// // 真偽値(Boolean)
+// // オブジェクト(Object)
+
+// console.log(typeof 'hello');
+// console.log(typeof 5);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+
+
+/* 定数と変数について */
+// let price = 500;
+
+// //price = price + 100;
+// price += 100; //600
+
+// //price = price * 2;
+// price *= 2; //1200
+
+// //price = price + 1;
+// //price += 1;
+// price++; //1201
+
+// //price -= 1;
+// price--; //1200
+
+// console.log(price);
+
+
+//定数 const
+//変数 let var
+//命名規則
+//英数字、$,_のみ
+//数値から始められない
+//大文字小文字は区別される
+//予約語は使えない（既にあるもの）
+
+
+// let price = 150;
+// console.log(price * 140);
+// console.log(price * 160);
+
+// price = 170;
+// console.log(price * 140);
+// console.log(price * 160);
+
+
+
+/* 比較演算子について */
+// console.log(10 + 3); // 13
+// console.log(10 - 3); // 7
+// console.log(10 * 3); // 30
+// console.log(10 / 3); // 3.333...
+// console.log(10 % 3); // 1
+// console.log(10 ** 3); // 1000
+
+// console.log(2 + 10 * 3); // 32
+// console.log((2 + 10) * 3); // 36
+
+
+
+/* console.logについて */
+// console.log("hello");
+// console.log("hello");
+
+// console.log('it's me!');
+// console.log("it's me!");
+// console.log("it\'s me!");
+
+// console.log('hel\nlo wor\tld');
+// // \nは改行、\tはスペース（タグ）をつけることができる
+
+// console.log('hello' + 'world');
+// // +は文字列同士を連結することができる
+
+
+
+/* コメントアウトについて */
+//コメント
+
+/*
+コメント
+コメント
+コメント
+*/
+
+//console.log("Hello World from main.js!");
+//コンソールに表示する命令
